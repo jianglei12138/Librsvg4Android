296dd83b1debb9079bb6548c0f612442d7e9357f
jianglei12138 <jianglei12138@gmail.com>
2019-10-21 12:57:22 +0800

    [feature] finish cross compile

---
+++ build/usr/local/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/jianglei/svg/build/usr/local/lib"
+XML2_LIBS="-lxml2 -L/home/jianglei/svg/build/usr/local/lib -lz-1-2-11    -liconv  -lm "
+XML2_INCLUDEDIR="-I/home/jianglei/svg/build/usr/local/include/libxml2"
+MODULE_VERSION="xml2-2.9.9"
+
---
+++ fontconfig-2.12.5/src/fcstdint.h
@@ -0,0 +1,9 @@
+#ifndef _FONTCONFIG_SRC_FCSTDINT_H
+#define _FONTCONFIG_SRC_FCSTDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "fontconfig 2.12.5"
+/* generated using gnu compiler aarch64-linux-android-gcc (GCC) 4.9.x 20150123 (prerelease) */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
